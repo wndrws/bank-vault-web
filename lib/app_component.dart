5f2457c42539c421a31046e964278abc2c4edde7
WebSter <wws.dev@gmail.com>
2018-12-01 23:06:16 +0300

    Created login component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:bank_vault/src/client_component.dart';
+import 'package:bank_vault/src/login_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -8,7 +9,7 @@ import 'package:bank_vault/src/client_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [ClientComponent],
+  directives: [ClientComponent, LoginComponent],
 )
 class AppComponent {
   // Nothing here yet.
---
+++ lib/src/login_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'login-component',
+  styleUrls: ['login_component.css'],
+  templateUrl: 'login_component.html',
+)
+class LoginComponent {
+
+}
