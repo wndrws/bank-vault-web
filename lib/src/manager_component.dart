599bc20fae73087da41ec022794351e44d2f864a
WebSter <wws.dev@gmail.com>
2018-12-12 05:07:38 +0300

    Created manager_component design

---
+++ lib/src/manager_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'manager-component',
+    styleUrls: ['manager_component.css'],
+    templateUrl: 'manager_component.html',
+    directives: [coreDirectives]
+)
+class ManagerComponent {
+
+}
